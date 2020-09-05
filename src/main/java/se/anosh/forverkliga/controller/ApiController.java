af862813d6a07883ae96456b5d09f5b100bd11d3
Anosh D. Ullenius <anosh@anosh.se>
2020-09-05 07:51:29 +0200

    Refactor controller. Add logger

--- src/main/java/se/anosh/forverkliga/controller/ApiController.java
+++ src/main/java/se/anosh/forverkliga/controller/ApiController.java
@@ -2,6 +2,8 @@ package se.anosh.forverkliga.controller;
 
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,12 @@ import se.anosh.forverkliga.service.BookService;
 public class ApiController {
 
 	private BookService service;
+	private final Logger logger;
 
 	@Autowired
 	public ApiController(BookService service) {
 		this.service = service;
+		logger = LoggerFactory.getLogger("ApiController.class");
 	}
 
 	@GetMapping(path="/", produces=MediaType.APPLICATION_JSON_VALUE)
@@ -33,7 +37,7 @@ public class ApiController {
 		return viewAllBooks();
 		} else if (op.contentEquals("update")) {
 			
-			System.out.println("running update...");
+			logger.debug("Running update");
 			Book updated = new Book();
 			updated.setId(Long.parseLong(id));
 			updated.setAuthor(author);
@@ -46,65 +50,72 @@ public class ApiController {
 		else if (op.contentEquals("insert")) {
 			
 			Book book = new Book(title, author);
-			System.out.println("Adding book: " + book);
+			logger.info("Adding book: {}", book);
 			service.addBook(book);
 			return addBook(book.getId());
 		}
 
+		return HttpFail();
+	}
+	
+	private ResponseEntity<BookWrapper> addBook(Long id) {
+		BookWrapper wrapper = new BookWrapper();
+		wrapper.setId(id);
+		return HttpOK(wrapper);
+	}
+	
+	private ResponseEntity<BookWrapper> removeBook(long id) {
+		service.removeBook(id);
+		BookWrapper wrapper = new BookWrapper();
+		return HttpOK(wrapper);
+	}
+	
+	private ResponseEntity<BookWrapper> viewAllBooks() {
+		BookWrapper wrapper = new BookWrapper(service.findAllBooks());
+
+		return HttpOK(wrapper);
+	}
+	
+	private ResponseEntity<BookWrapper> updateBook(Book updated) {
+		service.updateBook(updated.getId(), updated);
+		BookWrapper wrapper = new BookWrapper();
+
+		return HttpOK(wrapper);
+	}
+	
+	private ResponseEntity<BookWrapper> HttpOK(BookWrapper wrapper) {
+		return new ResponseEntity<BookWrapper>(wrapper,HttpStatus.OK);
+	}
+	
+	private ResponseEntity<BookWrapper> HttpFail() {
 		BookWrapper error = new BookWrapper();
 		error.setStatus("error");
 		error.setMessage("FAIL");
 		return new ResponseEntity<BookWrapper>(error,HttpStatus.I_AM_A_TEAPOT);
 	}
 	
-	private ResponseEntity<BookWrapper> addBook(Long id) {
-		BookWrapper wrapper = new BookWrapper();
-		wrapper.status = "success";
-		wrapper.setId(id);
-		return new ResponseEntity<BookWrapper>(wrapper,HttpStatus.OK);
-	}
-	
-	private ResponseEntity<BookWrapper> removeBook(long id) {
-		service.removeBook(id);
-		BookWrapper wrapper = new BookWrapper();
-		wrapper.status = "success";
-		return new ResponseEntity<BookWrapper>(wrapper,HttpStatus.OK);
-	}
-	
-	private ResponseEntity<BookWrapper> viewAllBooks() {
-		BookWrapper wrapper = new BookWrapper();
-		wrapper.data = service.findAllBooks();
-		wrapper.status = "success";
-
-		return new ResponseEntity<BookWrapper>(wrapper,HttpStatus.OK);
-	}
-	
-	private ResponseEntity<BookWrapper> updateBook(Book updated) {
-		System.out.println("updateBook received: " + updated);
-		service.updateBook(updated.getId(), updated);
-		BookWrapper wrapper = new BookWrapper();
-		wrapper.status = "success";
-
-		return new ResponseEntity<BookWrapper>(wrapper,HttpStatus.OK);
-	}
 
 	private static class BookWrapper {
 
 		public BookWrapper() {
+			status = "success";
 		}
+		
+		public BookWrapper(Collection<Book> data) {
+			this.data = data;
+			status = "success";
+		}
+		
 		@JsonInclude(Include.NON_NULL)
-		Collection<Book> data;
-		String status;
+		private Collection<Book> data;
+		private String status;
 		@JsonInclude(Include.NON_NULL)
-		String message;
+		private String message;
 		@JsonInclude(Include.NON_NULL)
-		Long id;
+		private Long id;
 		
 		public Collection<Book> getData() {
 			return data;
-		}
-		public void setData(Collection<Book> data) {
-			this.data = data;
 		}
 		public String getStatus() {
 			return status;
@@ -119,7 +130,6 @@ public class ApiController {
 			this.message = message;
 		}
 		public void setId(Long id) {
-			System.out.println("wrapper... settting id: " + id);
 			this.id = id;
 		}
 		
